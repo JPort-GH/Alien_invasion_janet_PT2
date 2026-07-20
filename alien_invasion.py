1149276b722c4b7afe0f0af37c3d270304e83b90
jport-gh <janet4portillo@gmail.com>
2026-07-19 23:20:06 -0400

    Part 2: Implement fleet size calculations, alien placement, and full fleet creation logic

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ Author: Janet Portillo
 GitHub: JPort-GH
 Course: CSCI-1511 – Python Programming
 Professor: Walters
-Assignment: Alien Invasion – Part 2 (Unit 7)
+Assignment: Alien Invasion – Part 2
 Date: 07.12.2026
 
 Description:
