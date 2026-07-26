851b42d5ac776892071db2fcd661e365bb158073
jport-gh <janet4portillo@gmail.com>
2026-07-26 01:32:57 -0400

    feat: add GameStats class with game_active, score, level, and lives

--- game_stats.py
+++ game_stats.py
@@ -7,13 +7,22 @@ Date: July 2026
 """
 
 class GameStats:
-    """Track game statistics for Alien Invasion."""
+    """Track statistics for Alien Invasion."""
 
-    def __init__(self, settings):
-        self.settings = settings
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False
+
+        # High score should never be reset.
+        self.high_score = 0
 
     def reset_stats(self):
-        """Initialize stats that change during the game."""
+        """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
+
