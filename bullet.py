90513dd7d4dc73ba56e697319e79def3a687fea0
jport-gh <janet4portillo@gmail.com>
2026-07-26 23:39:27 -0400

    bullet.py: change laser direction to shoot upward from ship

--- bullet.py
+++ bullet.py
@@ -11,7 +11,7 @@ import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    """A bullet fired horizontally from the ship."""
+    """A bullet fired vertically from the ship."""
 
     def __init__(self, ai_game):
         """Create a bullet at the ship's current position."""
@@ -20,18 +20,16 @@ class Bullet(Sprite):
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
-        # Bullet starts at ship mid-right
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
                                 self.settings.bullet_height)
-        self.rect.midleft = ai_game.ship.rect.midright
+        self.rect.midbottom = ai_game.ship.rect.midtop
 
-        # Store bullet's horizontal position
-        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
     def update(self):
-        """Move the bullet horizontally to the right."""
-        self.x += self.settings.bullet_speed
-        self.rect.x = self.x
+        """Move the bullet vertically upward."""
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
 
     def draw_bullet(self):
         """Draw the bullet to the screen."""
